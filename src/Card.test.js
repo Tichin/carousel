359381ce4be2068a50db00c42265bf19b40519bc
Tichin Wu <twu38@mail.ccsf.edu>
2023-08-22 16:43:22 -0700

    WIP first part

---
+++ src/Card.test.js
@@ -0,0 +1,24 @@
+import { render, fireEvent } from "@testing-library/react";
+import Card from "./Card";
+
+it("renders without crashing", function () {
+  render(<Card
+    caption='testing image 1'
+    src='test1.com'
+    currNum={1}
+    totalNum={1}
+  />);
+});
+
+it('matches snapshot', function () {
+
+  const { container } = render(<Card
+    caption='testing image 1'
+    src='test1.com'
+    currNum={1}
+    totalNum={1}
+  />);
+
+  expect(container).toMatchSnapshot();
+
+});
